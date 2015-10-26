6f8b6529e8ffce193ac422c945eb142e95a77fbc
Rafael Lima <rafaelnlima@live.com>
2015-10-26 11:45:46 -0300

    configucacao servidor

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => 'mysql.hostinger.com.br',
-            'database'  => 'u194446096_root',
-            'username'  => 'u194446096_rafa',
-            'password'  => '1966196089',
+            'host'      => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
