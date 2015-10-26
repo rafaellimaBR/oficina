dfcf50f6d53e5caef9885fe35b874f6d27fd2b04
Rafael Lima <rafaelnlima@live.com>
2015-10-26 11:57:02 -0300

    alteracao rodape

--- resources/views/admin/layout/lte/includes/rodape.blade.php
+++ resources/views/admin/layout/lte/includes/rodape.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-        <b>Version</b> 2.3.0
+        <b>Version</b> 1.0.0
     </div>
-    <strong>Copyright &copy; 2014-2015 <a href="">Rafael Lima</a>.</strong> Todos os direitos reservados.
+    <strong>Copyright &copy; 2014-2015 <a href="https://www.facebook.com/rafaelnlima">Rafael Lima</a>.</strong> Todos os direitos reservados.
 </footer>
